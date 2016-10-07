b44ca71fc9778c31ec5220dd0c744f040a780b20
Juan Marcelo <jarteaga@agetic.gob.bo>
2016-10-07 18:29:07 -0400

    constantes

--- app/scripts/services/constantes.js
+++ app/scripts/services/constantes.js
@@ -8,4 +8,4 @@
  * Constant in the sugerenciasApp.
  */
 angular.module('sugerenciasApp')
-  .constant('restUrl', 'http://192.168.21.154:8000/api/v2/') // Rest Django
+  .constant('restUrl', 'http://127.0.0.1:8000/api/v2/') // Rest Django
